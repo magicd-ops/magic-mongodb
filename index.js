3a4cc5096942e5df32ec2dc4f25b4ba410091f97
Magicd <magictest2020@gmail.com>
2020-12-27 11:52:33 +0330

    change some functions to private with # - bug fixed for use insert many instead of update many

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ class MongoDB extends MongoClient {
         };
         this.dbo = null;
         this.processCodes = []; // list of process codes that will add to name of created event listener
-        this.dbConnect();
+        this.#dbConnect();
     }
     response(listener){
         if(this.processCodes.length){
@@ -38,17 +38,17 @@ class MongoDB extends MongoClient {
      * this function will check if db is ready and then call current func
      * @param {function} func It will return selected function
      */
-    dbReady(func, data){
+    #dbReady(func, data){
         setTimeout(() => {
             if(!this.dbo){
-                this.dbReady(func, data);
+                this.#dbReady(func, data);
             } else {
                 func.call(this, ...data);
             }
         }, 100);
     }
 
-    syncQuery(query){
+    #syncQuery(query){
         query = query ? query : {};
         if(query._id){
             query._id = ObjectId(query._id);
@@ -56,18 +56,18 @@ class MongoDB extends MongoClient {
         return query;
     }
 
-    callConnection(){
+    #callConnection(){
         return this.connect({}, err => {
             if(err) throw err;
         });
     }
 
-    async dbConnect(){
-        await this.callConnection();
+    async #dbConnect(){
+        await this.#callConnection();
         this.dbo = this.db(this.opt.dbName); // select db and set to this.dbo
         // create tables from table list if not exist
         this.opt.collections.forEach(async collect => {
-            const exists = await this.checkExist(collect);
+            const exists = await this.#checkExist(collect);
             // check if table is not exist and create {collect} name of table
             if(!exists){
                 await this.dbo.createCollection(collect, (err, res) => {
@@ -78,17 +78,17 @@ class MongoDB extends MongoClient {
     }
 
     // check if table exist
-    async checkExist(collectionName){
+    async #checkExist(collectionName){
         return await(await this.dbo.listCollections().toArray()).findIndex((item) => item.name === collectionName) !== -1;
     }
 
-    createCode(){
+    #createCode(){
         let code = uuidv4();
         this.processCodes.push(code);
         return code;
     }
 
-    emitCall(code, result){
+    #emitCall(code, result){
         if(this.processCodes.indexOf(code) < 0){
             this.emit(`call-${code}`, result);
         } else {
@@ -96,16 +96,16 @@ class MongoDB extends MongoClient {
         }
     }
 
-    callGetData({code, collectionName, query, sort}){
+    #callGetData({code, collectionName, query, sort}){
         return this.dbo.collection(collectionName).find(query).sort(sort).toArray((err, result) => {
             if(err) throw err;
-            this.emitCall(code, result);
+            this.#emitCall(code, result);
         });
     }
 
-    updateGetOptions({options}){
+    #updateGetOptions({options}){
         if(!options) options = {};
-        options.query = this.syncQuery(options.query);
+        options.query = this.#syncQuery(options.query);
         if(!options.query) options.query = {};
         if(!options.sort) options.sort = {};
         return options;
@@ -117,42 +117,42 @@ class MongoDB extends MongoClient {
      * @param {object} options selected Query {query: ''}
     */
     async getData(collectionName, options){
-        let code = this.createCode();
+        let code = this.#createCode();
         if(this.dbo){
-            options = this.updateGetOptions({options});
+            options = this.#updateGetOptions({options});
             let { query, sort } = options;
-            const exists = await this.checkExist(collectionName);
+            const exists = await this.#checkExist(collectionName);
             if(exists){
                 // get data from selected table with query ( use {} when should get all data)
-                await this.callGetData({code, collectionName, query, sort});
+                await this.#callGetData({code, collectionName, query, sort});
             } else {
-                this.emitCall(code, false);
+                this.#emitCall(code, false);
             }
         } else {
             // if db is not ready yet , will call dbReady until this.dbo is ready
-            this.dbReady(this.getData, {collectionName, options});
+            this.#dbReady(this.getData, {collectionName, options});
         }
     }
 
-    callCreateDataWithInsertOne({code, collectionName, data}){
+    #callCreateDataWithInsertOne({code, collectionName, data}){
         return this.dbo.collection(collectionName).insertOne(data, (err, result) => {
             if(err) throw err;
-            this.emitCall(code, result.ops);
+            this.#emitCall(code, result.ops);
         });
     }
 
-    callCreateDataWithInsertMany({code, collectionName, data}){
+    #callCreateDataWithInsertMany({code, collectionName, data}){
         return this.dbo.collection(collectionName).insertMany(data, (err, result) => {
             if(err) throw err;
-            this.emitCall(code, result.ops);
+            this.#emitCall(code, result.ops);
         });
     }
 
-    async callCreateData({code, collectionName, options, data, length}){
+    async #callCreateData({code, collectionName, options, data, length}){
         if(!length){
-            await this.callCreateDataWithInsertOne({code, collectionName, options, data});
+            await this.#callCreateDataWithInsertOne({code, collectionName, options, data});
         } else {
-            await this.callCreateDataWithInsertMany({code, collectionName, options, data});
+            await this.#callCreateDataWithInsertMany({code, collectionName, options, data});
         }
     }
 
@@ -163,113 +163,113 @@ class MongoDB extends MongoClient {
      * @param {object} options
      */
     async createData(collectionName, data, options = {}){
-        let code = this.createCode();
+        let code = this.#createCode();
         let length = data.length;
         if(this.dbo){
-            const exists = await this.checkExist(collectionName);
+            const exists = await this.#checkExist(collectionName);
             if(exists){
-                await this.callCreateData({code, collectionName, options, data, length});
+                await this.#callCreateData({code, collectionName, options, data, length});
             } else {
-                this.emitCall(code, false);
+                this.#emitCall(code, false);
             }
         } else {
             // if db is not ready yet , will call dbReady until this.dbo is ready
-            this.dbReady(this.createData, {collectionName, data, options});
+            this.#dbReady(this.createData, {collectionName, data, options});
         }
     }
 
-    callUpdateDataWithUpdateOne({code, collectionName, options, data}){
+    #callUpdateDataWithUpdateOne({code, collectionName, options, data}){
         return this.dbo.collection(collectionName).updateOne(options.query, data, (err, {result}) => {
             if(err) throw err;
-            this.emitCall(code, result);
+            this.#emitCall(code, result);
         });
     }
 
-    callUpdateDataWithUpdateMany({code, collectionName, options, data}){
+    #callUpdateDataWithUpdateMany({code, collectionName, options, data}){
         return this.dbo.collection(collectionName).updateMany(options.query, data, (err, {result}) => {
             if(err) throw err;
-            this.emitCall(code, result);
+            this.#emitCall(code, result);
         });
     }
 
-    async callUpdateData({code, collectionName, options, data}){
+    async #callUpdateData({code, collectionName, options, data}){
         switch(options.type){
             case 'one':
-                await this.callUpdateDataWithUpdateOne({code, collectionName, options, data});
+                await this.#callUpdateDataWithUpdateOne({code, collectionName, options, data});
                 break;
             case 'many':
-                await this.callCreateDataWithInsertMany({code, collectionName, options, data});
+                await this.#callUpdateDataWithUpdateMany({code, collectionName, options, data});
         }
     }
 
-    updateUpdateOptions({options}){
+    #updateUpdateOptions({options}){
         options.type = options.type ? options.type : 'one'; // one , many
-        options.query = this.syncQuery(options.query);
+        options.query = this.#syncQuery(options.query);
         return options;
     }
 
     async updateData(collectionName, data, options = {}){
-        let code = this.createCode();
-        options = this.updateUpdateOptions({options});
+        let code = this.#createCode();
+        options = this.#updateUpdateOptions({options});
 
         data = { $set: data };
         if(this.dbo){
-            const exists = await this.checkExist(collectionName);
+            const exists = await this.#checkExist(collectionName);
             if(exists){
-                await this.callUpdateData({code, collectionName, options, data});
+                await this.#callUpdateData({code, collectionName, options, data});
             } else {
-                this.emitCall(code, false);
+                this.#emitCall(code, false);
             }
         } else {
             // if db is not ready yet , will call dbReady until this.dbo is ready
-            this.dbReady(this.updateData, {collectionName, data, options});
+            this.#dbReady(this.updateData, {collectionName, data, options});
         }
     }
 
-    updateDeleteOptions({options}){
+    #updateDeleteOptions({options}){
         options.type = options.type ? options.type : 'one'; // one , many
-        options.query = this.syncQuery(options.query);
+        options.query = this.#syncQuery(options.query);
         return options;
     }
 
-    callDeleteDataWithDeleteOne({code, collectionName, options}){
+    #callDeleteDataWithDeleteOne({code, collectionName, options}){
         return this.dbo.collection(collectionName).deleteOne(options.query, (err, {result}) => {
             if(err) throw err;
-            this.emitCall(code, result);
+            this.#emitCall(code, result);
         });
     }
 
-    callDeleteDataWithDeleteMany({code, collectionName, options}){
+    #callDeleteDataWithDeleteMany({code, collectionName, options}){
         return this.dbo.collection(collectionName).deleteMany(options.query, (err, {result}) => {
             if(err) throw err;
-            this.emitCall(code, result);
+            this.#emitCall(code, result);
         });
     }
 
-    async callDeleteData({code, collectionName, options}){
+    async #callDeleteData({code, collectionName, options}){
         switch(options.type){
             case 'one':
-                await this.callDeleteDataWithDeleteOne({code, collectionName, options});
+                await this.#callDeleteDataWithDeleteOne({code, collectionName, options});
                 break;
             case 'many':
-                await this.callDeleteDataWithDeleteMany({code, collectionName, options});
+                await this.#callDeleteDataWithDeleteMany({code, collectionName, options});
         }
     }
 
 	async deleteData(collectionName, options = {}){
-        let code = this.createCode();
-        options = this.updateDeleteOptions({options});
+        let code = this.#createCode();
+        options = this.#updateDeleteOptions({options});
 
         if(this.dbo){
-            const exists = await this.checkExist(collectionName);
+            const exists = await this.#checkExist(collectionName);
             if(exists){
-                await this.callDeleteData({code, collectionName, options});
+                await this.#callDeleteData({code, collectionName, options});
             } else {
-                this.emitCall(code, false);
+                this.#emitCall(code, false);
             }
         } else {
             // if db is not ready yet , will call dbReady until this.dbo is ready
-            this.dbReady(this.deleteData, {collectionName, options});
+            this.#dbReady(this.deleteData, {collectionName, options});
         }
     }
 }
